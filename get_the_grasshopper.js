2a702393077854c9afb238d51aef42f277c0bb80
André Coutinho de Macedo Silva <91290799+AndreCoutinhom@users.noreply.github.com>
2023-06-03 01:47:02 -0300

    Create get_the_grasshopper.js

---
+++ get_the_grasshopper.js
@@ -0,0 +1,23 @@
+// -- EXERCISE 9 *GET THE GRASSHOPPER* --
+// The exercise comes like this
+
+let bugs = [
+    ['bee', 'ant'],
+    ['cricket', 'grasshopper'],
+    ['ladybug', 'mantis'],
+];
+
+console.log(bugs[2][0]);
+
+// It should get to this
+
+let bugs = [
+    ['bee', 'ant'],
+    ['cricket', 'grasshopper'],
+    ['ladybug', 'mantis'],
+];
+
+console.log(bugs[1][1]);
+
+// -- ADDITIONAL INFO --
+// https://www.google.com/script/start/
